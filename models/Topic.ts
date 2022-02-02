dc9b45d266b26f4e5d359ec7438d9f5b9d43667d
tgreizer <greizer.t@northeasetern.edu>
2022-02-02 13:18:38 -0500

    from work

---
+++ models/Topic.ts
@@ -0,0 +1,6 @@
+import Tuit2Topic from "./Tuit2Topic";
+
+export default class Topic {
+  private topic: string;
+  private tuit2Topic: Tuit2Topic[];
+};
---
+++ models/Tuit2Tag.ts
@@ -0,0 +1,7 @@
+import Tuit from "./Tuit";
+import Tag from "./Tag";
+
+export default class Tuit2Tag {
+  private tag: Tag;
+  private tuit: Tuit;
+};
