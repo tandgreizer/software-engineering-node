be3b1d84021d5e4bf56b99d4a61a633aa650a752
tgreizer <greizer.t@northeasetern.edu>
2022-02-04 10:29:23 -0500

    from work

--- daos/UserDao.ts
+++ daos/UserDao.ts
@@ -2,21 +2,21 @@ import User from "../models/User";
 import UserModel from "../mongoose/UserModel";
 import UserDaoI from "../interfaces/UserDao";
 
-export default class UserDao implements UserDaoI {
-  async findAllUsers(): Promise<User[]> {
-    return await UserModel.find();
-  }
-  async findUserById(uid: string): Promise<User> {
-    return await UserModel.findById(userId);
-  }
-  async createUser(user: User): Promise<void> {
-    return await UserModel.create(user);
-  }
-  async deleteUser(uid: string):  Promise<any> {
-    return await UserModel.deleteOne({_id: userId});
-  }
-  async updateUser(uid: string, user: User): Promise<any> {
-    return await UserModel.updateOne({_id: userId}, {$set: user});
-  }
-}
-
+// export default class UserDao implements UserDaoI {
+//   async findAllUsers(): Promise<User[]> {
+//     return await UserModel.find();
+//   }
+//   async findUserById(uid: string): Promise<User> {
+//     return await UserModel.findById(userId);
+//   }
+//   async createUser(user: User): Promise<void> {
+//     return await UserModel.create(user);
+//   }
+//   async deleteUser(uid: string):  Promise<any> {
+//     return await UserModel.deleteOne({_id: userId});
+//   }
+//   async updateUser(uid: string, user: User): Promise<any> {
+//     return await UserModel.updateOne({_id: userId}, {$set: user});
+//   }
+// }
+//
