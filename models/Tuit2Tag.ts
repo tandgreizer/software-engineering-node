8e9c5c544b1b6e56dd183f05ee62a80fddba5b86
tagreizer <greizer.t@northeastern.edu>
2022-02-25 16:12:48 -0500

    committing mid work

--- models/Tuit2Tag.ts
+++
@@ -1,8 +0,0 @@
-import Tuit from "./Tuit";
-import Tag from "./Tag";
-import * as mongoose from "mongoose";
-
-export default class Tuit2Tag extends mongoose.SchemaType {
-  private tag: Tag = new Tag();
-  private tuit: Tuit = new Tuit();
-};
--- models/Tuit2Topic.ts
+++
@@ -1,8 +0,0 @@
-import Topic from "./Topic";
-import Tuit from "./Tuit";
-import mongoose from "mongoose";
-
-export default class Tuit2Topic extends mongoose.SchemaType{
-  private topic: Topic = new Topic();
-  private tuit: Tuit = new Tuit();
-};
